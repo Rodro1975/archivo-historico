6f6850826724fee7879616cc9521206a2cf10113
Rodrigo Ordoñez <rodrigoordonez@nube.unadmexico.mx>
2024-10-13 21:11:32 -0500

    Se crea el componente carrusel para la sección de noticias y eventos

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 // app/page.js
-"use client"; // Esto asegura que este componente puede usar hooks si es necesario
+"use client";
 
-import Head from "next/head"; // Asegúrate de importar Head
-import SocialBar from "../components/SocialBar"; // Importa la barra de redes sociales
-import NavBar from "../components/NavBar"; // Asegúrate de que la ruta sea correcta
-import Image from "next/image"; // Asegúrate de importar Image
-import Link from "next/link"; // Asegúrate de importar Link
-import { useEffect, useState } from "react"; // Importa useEffect y useState para manejar el estado
-import Modal from "../components/Modal"; // Importa el componente Modal
+import Head from "next/head";
+import SocialBar from "../components/SocialBar";
+import NavBar from "../components/NavBar";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
 import Footer from "../components/Footer";
+import Carrusel from "../components/Carrusel";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,10 +17,10 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsModalOpen(true);
-    }, 2000); // Abre el modal después de 2 segundos
+    }, 2000);
 
     const handleScroll = () => {
-      setIsModalOpen(false); // Cierra el modal si el usuario se desplaza
+      setIsModalOpen(false);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -30,36 +31,35 @@ export default function Home() {
     };
   }, []);
 
+  const slides = [
+    { image: "/images/libroUg.jpg", alt: "Descripción de la imagen 1" },
+    { image: "/images/libroUg.jpg", alt: "Descripción de la imagen 2" },
+    { image: "/images/libroUg.jpg", alt: "Descripción de la imagen 3" },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Head>
         <title>Página Principal - Archivo Histórico Librería UG</title>
       </Head>
-      {/* Agrega la barra de redes sociales */}
       <SocialBar />
-      {/* Hero Section */}
       <section
         className="relative py-48 text-center bg-fixed bg-cover bg-center"
         style={{ backgroundImage: "url('/images/guanajuato.jpg')" }}
       >
-        <div className="absolute inset-0 bg-black opacity-40"></div>{" "}
-        {/* Overlay oscuro para mejorar contraste */}
-        {/* Escudo de la Universidad */}
+        <div className="absolute inset-0 bg-black opacity-40"></div>
         <Image
           src="/images/escudo-png.png"
           alt="Escudo UG"
-          width={256} // Requerido por Next.js
-          height={256} // Requerido por Next.js
-          className="mx-auto w-64 h-64 mb-6 animate__animated animate__bounce" // Añadir animación al escudo
+          width={256}
+          height={256}
+          className="mx-auto w-64 h-64 mb-6"
         />
-        {/* Overlay */}
         <div className="relative z-10 container mx-auto">
           <h1 className="text-gold text-6xl md:text-8xl font-extrabold leading-tight drop-shadow-lg">
             Archivo Histórico de la Librería UG
           </h1>
-          <p
-            className={`text-gold mt-4 text-xl md:text-2xl animate__animated animate__bounceInDown`}
-          >
+          <p className="text-gold mt-4 text-xl md:text-2xl">
             Explora nuestra colección y aprende más sobre la historia de la
             universidad.
           </p>
@@ -72,15 +72,14 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Barra de navegación */}
       <NavBar />
-
-      {/* Mostrar modal */}
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
 
-      <section className="news-events">
-        <h2>Noticias y Eventos</h2>
-        {/* Noticias Recientes */}
+      <section className="news-events relative py-48 text-center bg-fixed bg-cover bg-center">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          Noticias y Eventos
+        </h2>
+        <Carrusel slides={slides} />
       </section>
 
       <section className="collections-highlighted py-8">
