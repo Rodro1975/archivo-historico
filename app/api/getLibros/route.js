b1daf29c35c4a991915b12c23ee6901b71a131b7
Rodrigo Ordoñez <rodrigoordonez@nube.unadmexico.mx>
2024-11-23 13:23:01 -0500

    Se crea archivo getLibros para conectar a la tabla libros

---
+++ app/api/getLibros/route.js
@@ -0,0 +1,21 @@
+import supabase from "@/supabase"; // Importa la instancia configurada de supabase
+
+export async function GET() {
+  try {
+    const { data, error } = await supabase.from("libros").select("*");
+
+    if (error) {
+      return new Response(
+        JSON.stringify({ message: "Error al obtener los libros", error }),
+        { status: 500 }
+      );
+    }
+
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "Error al procesar la solicitud", error }),
+      { status: 500 }
+    );
+  }
+}
